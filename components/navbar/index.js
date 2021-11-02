bbf18b6c163b36f0f73d1b4b28a0142c4ba85aa9
Hugyn <hugoncc@hotmail.com>
2021-11-02 16:20:07 +0000

    nav

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Image from 'next/image'
-import './Navbar.module.css'
+import styles from './Navbar.module.css'
+
 
 function Navbar() {
     return (
-        <div>
-            <Image src="/logo.svg" height={36} width={73}/>
+        <div className={styles.navbar}>
+            <Image src="/logo.svg" height={36} width={73} alt="The best foodie experience awesome logo" />
+            <ul className={styles.leftMenu}>
+                <li>LINKS</li>
+                <li>LINKS</li>
+                <li>LINKS</li>
+                <li>LINKS</li>
+                <li>LINKS</li>
+            </ul>
+
+            <div className={styles.rightMenu}>
+                <ul className={styles.navRight}>
+                    <li>Contact</li>
+                    <li>Log in</li>
+                </ul>
+            </div>
         </div>
     )
 }
