7b6e4392f2475772b95ad059e9a6dc66e84a56e7
Hugyn <hugoncc@hotmail.com>
2021-11-03 17:22:00 +0000

    comit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.home}>
         <div className={styles.homeHero}>
-          <h1>The best foodie <br/><span>experience</span></h1>
+          <h1>The best foodie<br/><span>experience</span></h1>
           <h3>now in London</h3>
         </div>
     </div>
