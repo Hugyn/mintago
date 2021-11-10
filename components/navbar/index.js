70b7843dd6cb874c3ea06b09e13a7beb04150e8f
Hugyn <hugoncc@hotmail.com>
2021-11-10 11:51:41 +0000

    commit

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -8,7 +8,7 @@ import { useRouter } from 'next/router'
 
 function Navbar() {
     const router = useRouter()
-    const navLinks = ["about", "community", "location", "our menu", "recipes"]
+    const navLinks = ["about", "community", "location", "our menu", "recipeeeeees"]
     
     function test(){
         router.push('/?step=2', null, { shallow: true })
